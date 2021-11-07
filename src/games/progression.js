4fffd123c78f4254e9e51566c20be3f1b8bfc4ed
kaguya322 <maksimanisimov322@gmail.com>
2021-11-07 11:23:09 +0200

    added progression game

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import { randomNumber } from '../random.js';
+import processGameRound from './index.js';
+
+const progressionLength = 10;
+
+const generateProgression = (difference, first, length) => {
+  const iter = (acc) => {
+    if (acc.length === length) return acc;
+
+    return iter([...acc, acc[acc.length - 1] + difference]);
+  };
+
+  return iter([first]);
+};
+
+const progressionGameRound = () => {
+  const difference = randomNumber(1, 10);
+  const first = randomNumber(0, 100);
+  const progression = generateProgression(difference, first, progressionLength);
+  const hiddenIndex = randomNumber(0, progressionLength - 1);
+  const hiddenElement = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+
+  console.log(`Question: ${progression.join(' ')}`);
+
+  return String(hiddenElement);
+};
+
+const progression = () => {
+  processGameRound(progressionGameRound, 'What number is missing in the progression?');
+};
+
+export default progression;
