1ab96d3bca102f79fbdcc45981b0f1f064346cfa
kaguya322 <maksimanisimov322@gmail.com>
2021-11-07 11:01:10 +0200

    added gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,21 @@
+import { randomNumber } from '../random.js';
+import processGameRound from './index.js';
+
+const getGcd = (a, b) => (b ? getGcd(b, a % b) : a);
+
+const gcdGameRound = () => {
+  const number1 = randomNumber(1, 100);
+  const number2 = randomNumber(1, 100);
+
+  const gcd = getGcd(number1, number2);
+
+  console.log(`Question: ${number1} ${number2}`);
+
+  return String(gcd);
+};
+
+const gcd = () => {
+  processGameRound(gcdGameRound, 'Find the greatest common divisor of given numbers.');
+};
+
+export default gcd;
