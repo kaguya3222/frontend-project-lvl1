e6a7ddddedf6630f99823494f46cdb62469dc0a2
kaguya322 <maksimanisimov322@gmail.com>
2021-11-24 16:25:37 +0200

    refactor: created getQuestion function in progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,27 +1,38 @@
 import getRandomNumber from '../random.js';
 import runGame from '../index.js';
 
-const generateProgression = (difference, first, length) => {
-  const iter = (acc) => {
-    if (acc.length === length) return acc;
+const progressionLength = 10;
+const hiddenElementCharacter = '..';
 
-    return iter([...acc, acc[acc.length - 1] + difference]);
+const getQuestion = (difference, first, missingIndex) => {
+  const getQuestionProgression = (acc, index) => {
+    if (acc.length === progressionLength) return acc;
+
+    const currentElement = acc[index];
+    const nextElement = currentElement + difference;
+    const nextIndex = index + 1;
+
+    if (index === missingIndex) {
+      return getQuestionProgression(
+        [...acc.slice(0, -1), hiddenElementCharacter, nextElement],
+        nextIndex,
+      );
+    }
+
+    return getQuestionProgression([...acc, nextElement], nextIndex);
   };
 
-  return iter([first]);
-};
+  const progression = getQuestionProgression([first], 0);
 
-const progressionLength = 10;
+  return progression.join(' ');
+};
 
 const runProgressionGameRound = () => {
   const difference = getRandomNumber(1, 10);
   const first = getRandomNumber(0, 100);
-  const progression = generateProgression(difference, first, progressionLength);
-  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
-  const progressionWithHiddenElement = progression.map((el, index) => (index === hiddenIndex ? '..' : el));
-  const hiddenElement = progression[hiddenIndex];
-  const expectedAnswer = String(hiddenElement);
-  const question = `${progressionWithHiddenElement.join(' ')}`;
+  const missingIndex = getRandomNumber(0, progressionLength - 1);
+  const question = getQuestion(difference, first, missingIndex);
+  const expectedAnswer = String(first + difference * missingIndex);
 
   return [expectedAnswer, question];
 };
